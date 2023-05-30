9b9e89b29331491f17e13356783cd16e8f0828c8
JKY99 <jkypch@gmail.com>
2023-05-30 23:13:57 +0900

    db uddate

--- database/beer.py
+++ database/beer.py
@@ -4,34 +4,20 @@ from bson.objectid import ObjectId
 from database.connection import db
 
 class BeerService:
-    # MongoDB에 있는 'Beers'라는 이름의 컬렉션을 참조하도록 설정합니다.
     collection = db['Beers']
 
     @classmethod
     async def create(cls, beer: Beer) -> Beer:
-        # Beer 객체를 dictionary 형태로 변환합니다.
         beer_dict = beer.dict()
-
-        # MongoDB에서는 "_id"라는 필드를 사용하여 고유한 ID를 관리합니다.
-        # 이를 위해 ObjectId를 생성하여 "_id" 필드에 할당합니다.
         beer_dict["_id"] = ObjectId()
-
-        # 생성된 Beer 객체를 'beers' 컬렉션에 삽입합니다.
         await cls.collection.insert_one(beer_dict)
-
-        # 생성된 Beer 객체를 반환합니다.
         return beer
 
     @classmethod
     async def read(cls, beer_id: str) -> Beer:
-        # 주어진 ID를 가진 Beer를 'beers' 컬렉션에서 찾습니다.
-        # 여기서 ID는 ObjectId 형태이므로 str 형태의 beer_id를 ObjectId로 변환합니다.
-        beer = await cls.collection.find_one({"beer_id": ObjectId(beer_id)})
-
-        # 찾은 Beer를 Beer 객체로 변환하여 반환하거나, 찾지 못한 경우 None을 반환합니다.
+        beer = await cls.collection.find_one({"beer_id": beer_id})
         return Beer(**beer) if beer else None
     
-    # BeerService in beer.py
     @classmethod
     async def read_by_name(cls, beer_name: str) -> Beer:
         beer = await cls.collection.find_one({"name": beer_name})
@@ -39,23 +25,14 @@ class BeerService:
 
     @classmethod
     async def read_all(cls) -> List[Beer]:
-        # 'beers' 컬렉션의 모든 Beer를 찾습니다. 
-        # 이 예제에서는 최대 100개의 Beer를 반환하도록 설정하였습니다.
         beers = await cls.collection.find().to_list(length=100)
-
-        # 찾은 모든 Beer를 Beer 객체로 변환하여 리스트 형태로 반환합니다.
         return [Beer(**beer) for beer in beers]
 
     @classmethod
     async def update(cls, beer_id: str, beer: Beer) -> Beer:
-        # 주어진 ID를 가진 Beer를 새로운 Beer로 업데이트합니다.
-        # 이때 ID는 ObjectId 형태이므로 str 형태의 beer_id를 ObjectId로 변환합니다.
-        await cls.collection.replace_one({"beer_id": ObjectId(beer_id)}, beer.dict())
-
-        # 업데이트된 Beer를 반환합니다.
+        await cls.collection.replace_one({"beer_id": beer_id}, beer.dict())
         return await cls.read(beer_id)
 
     @classmethod
     async def delete(cls, beer_id: str) -> None:
-        # 주어진 ID를 가진 Beer를 'beers' 컬렉션에서 삭제합니다.
-        await cls.collection.delete_one({"beer_id": ObjectId(beer_id)})
+        await cls.collection.delete_one({"beer_id": beer_id})
--- database/user.py
+++ database/user.py
@@ -5,43 +5,30 @@ from bson.objectid import ObjectId
 
 # 사용자에 대한 CRUD 연산을 수행하는 UserService 클래스입니다.
 class UserService:
-    # MongoDB의 users 컬렉션을 참조합니다.
     collection = db['Users']
 
-    # 새로운 사용자를 생성하고 반환하는 메서드입니다.
     @classmethod
     async def create(cls, user: User) -> User:
-        user_dict = user.dict()  # User pydantic 모델을 딕셔너리로 변환합니다.
-        user_dict["_id"] = ObjectId()  # 새로운 MongoDB ObjectId를 생성합니다.
-        await cls.collection.insert_one(user_dict)  # users 컬렉션에 새 문서를 삽입합니다.
-        return user  # 생성된 User 모델을 반환합니다.
+        user_dict = user.dict()
+        user_dict["_id"] = ObjectId()
+        await cls.collection.insert_one(user_dict)
+        return user
 
-    # 주어진 ID에 해당하는 사용자를 찾고 반환하는 메서드입니다.
     @classmethod
     async def read(cls, user_id: str) -> User:
-        # users 컬렉션에서 _id 필드가 user_id와 일치하는 문서를 찾습니다.
-        user = await cls.collection.find_one({"user_id": ObjectId(user_id)})
-        # 찾은 문서가 있으면 User 모델로 변환하고 반환하고, 없으면 None을 반환합니다.
+        user = await cls.collection.find_one({"user_id": user_id})
         return User(**user) if user else None
 
-    # 모든 사용자를 찾아 반환하는 메서드입니다.
     @classmethod
     async def read_all(cls) -> List[User]:
-        # users 컬렉션의 모든 문서를 찾아 최대 100개까지 리스트로 변환합니다.
         users = await cls.collection.find().to_list(length=100)
-        # 각 문서를 User 모델로 변환하여 리스트로 만들고 반환합니다.
         return [User(**user) for user in users]
 
-    # 주어진 ID에 해당하는 사용자의 정보를 업데이트하고 업데이트된 사용자를 반환하는 메서드입니다.
     @classmethod
     async def update(cls, user_id: str, user: User) -> User:
-        # users 컬렉션에서 _id 필드가 user_id와 일치하는 문서를 user의 정보로 교체합니다.
-        await cls.collection.replace_one({"user_id": ObjectId(user_id)}, user.dict())
-        # 업데이트된 사용자를 찾아서 반환합니다.
+        await cls.collection.replace_one({"user_id": user_id}, user.dict())
         return await cls.read(user_id)
 
-    # 주어진 ID에 해당하는 사용자를 삭제하는 메서드입니다.
     @classmethod
     async def delete(cls, user_id: str) -> None:
-        # users 컬렉션에서 _id 필드가 user_id와 일치하는 문서를 삭제합니다.
-        await cls.collection.delete_one({"user_id": ObjectId(user_id)})
+        await cls.collection.delete_one({"user_id": user_id})
